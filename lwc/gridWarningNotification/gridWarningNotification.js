4c1342f0306f7e645b5725362a233b03a058945f
AESESSBAI <aessbai@salesforce.com>
2023-12-20 15:18:40 +0100

    Delete lwc/gridWarningNotification directory

--- lwc/gridWarningNotification/gridWarningNotification.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from "lwc";
-
-export default class GridWarningNotification extends LightningElement {
-  @api message;
-
-  get showNotification() {
-    return this.message !== undefined && this.message !== "";
-  }
-}
